d43dea14a51d5973df4aaa6954ac5e0582667d27
osamaline1229 <osama98138139@gmail.com>
2023-07-06 00:36:29 -0700

    for-each

--- interface/Program.cs
+++ interface/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] a = {1, 2,3,4,5,6,67,8,9,10};
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+        Console.WriteLine("printing for each");
+        foreach(int i in a)
+        {
+            Console.WriteLine(i);
+        }
+
+
     }
 }
