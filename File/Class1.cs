f382f28cde6741a38f36a718fafddb97a1def876
osamaline1229 <osama98138139@gmail.com>
2023-07-06 01:37:17 -0700

    file

---
+++ File/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File
+{
+    internal class Class1
+    {
+    }
+    enum AgeGroup
+    {
+        CHILD = 17,
+        TEENAGER = 16,
+        ADULT = 18,
+        SENIOR = 49
+
+
+    }
+    enum Character
+    {
+        SAILM,
+        MOHAMMED,
+        AHMED,
+        OMRAN
+    }
+}
---
+++ File/Program.cs
@@ -0,0 +1,14 @@
+﻿using File;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = Character.MOHAMMED.ToString();
+        int Age = ((int)AgeGroup.TEENAGER);
+
+        Console.WriteLine($"His name {name} and his Age {Age}");
+
+    }
+}
