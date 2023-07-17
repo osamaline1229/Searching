d95fa5f80b5a1984ffce1d8271b58726c55ca20b
osamaline1229 <osama98138139@gmail.com>
2023-07-17 00:13:56 -0700

    Balance brackets

---
+++ SEARCHING/BracketChecker.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class BracketChecker
+{
+    public bool CheckBalance(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (IsOpeningBracket(c))
+            {
+                stack.Push(c);
+            }
+            else if (IsClosingBracket(c))
+            {
+                if (stack.Count == 0 || !AreMatchingBrackets(stack.Peek(), c))
+                {
+                    return false;
+                }
+
+                stack.Pop();
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    private bool IsOpeningBracket(char c)
+    {
+        return c == '(' || c == '{' || c == '[';
+    }
+
+    private bool IsClosingBracket(char c)
+    {
+        return c == ')' || c == '}' || c == ']';
+    }
+
+    private bool AreMatchingBrackets(char openingBracket, char closingBracket)
+    {
+        return (openingBracket == '(' && closingBracket == ')') ||
+               (openingBracket == '{' && closingBracket == '}') ||
+               (openingBracket == '[' && closingBracket == ']');
+    }
+}
+
+
+//            public int top = -1;
+//            public char[] items = new char[100];
+
+//            public void push(char x)
+//            {
+//                if (top == 99)
+//                {
+//                    Console.WriteLine("Stack full");
+//                }
+//                else
+//                {
+//                    items[++top] = x;
+//                }
+//            }
+
+//            char pop()
+//            {
+//                if (top == -1)
+//                {
+//                    Console.WriteLine("Underflow error");
+//                    return '\0';
+//                }
+//                else
+//                {
+//                    char element = items[top];
+//                    top--;
+//                    return element;
+//                }
+//            }
+
+//            bool isEmpty()
+//            {
+//                return (top == -1) ? true : false;
+//            }
+//        }
+
+
+//        public bool isMatchingPair(char character1, char character2)
+//        {
+//            if (character1 == '(' && character2 == ')')
+//                return true;
+//            else if (character1 == '{' && character2 == '}')
+//                return true;
+//            else if (character1 == '[' && character2 == ']')
+//                return true;
+//            else
+//                return false;
+//        }
+
+//    public bool Balanced(char[] exp)
+//    {
+
+//        Stack<char> st = new Stack<char>();
+
+
+//        for (int i = 0; i < exp.Length; i++)
+//        {
+
+//            if (exp[i] == '{' || exp[i] == '('
+//                || exp[i] == '[')
+//                st.Push(exp[i]);
+
+
+//            if (exp[i] == '}' || exp[i] == ')'
+//                || exp[i] == ']')
+//            {
+
+
+//                if (st.Count == 0)
+//                {
+//                    return false;
+//                }
+
+
+//                else if (!isMatchingPair(st.Pop(),
+//                                         exp[i]))
+//                {
+//                    return false;
+//                }
+//            }
+//        }
+
+
+
+//        if (st.Count == 0)
+//            return true;
+//        else
+//        {
+
+//            return false;
+//        }
+//    }
+
+
+
+
+//}
+
---
+++ SEARCHING/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    
+
+    static void Main(string[] args)
+    {
+
+        BracketChecker bracketChecker = new BracketChecker();
+
+        
+        string input1 = "((()])";           
+        string input2 = "{[()]}";           
+        string input3 = "([{}])";           
+        string input4 = "((()))))";         
+        string input5 = "[[{(())}]]";       
+
+        Console.WriteLine($"Input1: {bracketChecker.CheckBalance(input1)}"); 
+        Console.WriteLine($"Input2: {bracketChecker.CheckBalance(input2)}"); 
+        Console.WriteLine($"Input3: {bracketChecker.CheckBalance(input3)}"); 
+        Console.WriteLine($"Input4: {bracketChecker.CheckBalance(input4)}"); 
+        Console.WriteLine($"Input5: {bracketChecker.CheckBalance(input5)}"); 
+
+
+
+
+
+
+
+    }
+}
